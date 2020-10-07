ce4fedc81d1bbe68c477e48955f9f5508374ba99
Sébastien Henwood <sebwood@cedar1.cedar.computecanada.ca>
2020-10-07 13:41:38 -0700

    doc + licence

--- FaultyMemory/__init__.py
+++ FaultyMemory/__init__.py
@@ -1,14 +1,14 @@
 """Root package info."""
 
 __version__ = '0.0.1'
-__author__ = 'Victor Gaudreau-Blouin et al.'
+__author__ = 'Victor Gaudreau-Blouin, Sébastien Henwood'
 __author_email__ = 'noreply@polymtl.ca'
 __license__ = 'Apache-2.0'
 __copyright__ = 'Copyright (c) 2020, %s.' % __author__
 __homepage__ = 'https://github.com/VictorGeebs/FaultyMemory'
 
 __docs__ = (
-    "FaultyMemory is a lightweight wrapper to simulate the effect of faulty memories on Pytorch models."
+    "FaultyMemory is a lightweight wrapper to simulate the effect of hardware architecture on Pytorch models."
 )
 __long_docs__ = """
 In real-world scenario a deep learning model may encounter unexpected bit-flips.
