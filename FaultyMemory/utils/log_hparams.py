a2f2e8eb9ab3f210905a0383952a81e0053d7067
Lint Action <lint-action@samuelmeuli.com>
2021-05-20 14:32:34 +0000

    Fix code style issues with Black

--- FaultyMemory/utils/log_hparams.py
+++ FaultyMemory/utils/log_hparams.py
@@ -17,12 +17,13 @@ def log_hyperparameters(method_or_class):
         class UpdatedCls(method_or_class):
             def __init__(self, *args, **kwargs) -> None:
                 argnames = method_or_class.__init__.__code__.co_varnames[
-                    1:method_or_class.__init__.__code__.co_argcount
+                    1 : method_or_class.__init__.__code__.co_argcount
                 ]
                 argnames = dict(zip(argnames, args))
                 [argnames.pop(k, None) for k in ARGS_SKIP_LIST]
                 super().__init__(*args, **kwargs)
                 self._hyperparameters = {**argnames, **kwargs}
+
         UpdatedCls._hparam_logger = True
         return UpdatedCls
     else:
@@ -36,5 +37,6 @@ def log_hyperparameters(method_or_class):
             res = method_or_class(*args, **kwargs)
             res._hyperparameters = {**argnames, **kwargs}
             return res
+
         updated_method._hparam_logger = True
         return updated_method
